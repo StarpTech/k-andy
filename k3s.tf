4bc6c412ff44a74a9277356b4340b7724ca88cd5
starptech <deusdustin@gmail.com>
2021-03-07 22:58:46 +0100

    use cpx21 as agent default

--- k3s.tf
+++ k3s.tf
@@ -1,7 +1,7 @@
 locals {
   server_location           = "nbg1"
   control_plane_server_type = "cx11"
-  agent_server_type         = "cx21"
+  agent_server_type         = "cpx21"
   first_control_plane_ip    = cidrhost(hcloud_network_subnet.k3s_nodes.ip_range, 2)
 }
 

7729a9f0d26d60a17cf756db354a2657eb1ce5e0
Tobias Birmili <birmili@mecodia.de>
2021-07-28 09:31:46 +0200

    docs: add info about contributing

--- example/main.tf
+++ example/main.tf
@@ -3,7 +3,7 @@ variable "hcloud_token" {}
 module "demo_cluster" {
   source = "./.."
   # Can also point to a git repository, e.g. git::https://github.com/toabi/k-andy.git?ref=feature/modularize
-  hcloud_token = var.hcloud_token
-  name = "demo"
+  hcloud_token     = var.hcloud_token
+  name             = "demo"
   server_locations = ["nbg1", "fsn1"]
 }
